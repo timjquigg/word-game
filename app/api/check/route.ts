b8958a5e92635a0a9f5a0be8b52ad03c2c95da56
Tim Quigg <timjquigg@gmail.com>
2023-04-04 19:46:15 -0600

    add console log in check route

--- app/api/check/route.ts
+++ app/api/check/route.ts
@@ -2,8 +2,9 @@ import { NextResponse } from "next/server";
 import validate from "@/lib/validateWord";
 
 export async function POST(req: Request) {
-  const key = process.env.DICTIONARY_KEY;
+  // const key = process.env.DICTIONARY_KEY;
   const word = await req.json();
+  console.log(word);
   const valid = await validate(word);
   return NextResponse.json({ valid });
 }
