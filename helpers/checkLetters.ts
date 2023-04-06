49193a3a4c1ed758f6ef8aea31c19c6633965503
Tim Quigg <timjquigg@gmail.com>
2023-04-06 10:31:49 -0600

    Update keyboard to show absent values

--- helpers/checkLetters.ts
+++ helpers/checkLetters.ts
@@ -2,18 +2,23 @@ export default function checkLetters(
   attempt: LetterCheck[],
   keys: LetterCheck
 ): LetterCheck {
-  const updatedKeys: LetterCheck = {};
+  const updatedKeys: LetterCheck = { ...keys };
 
   attempt.forEach((letter, index) => {
+    if (Object.values(letter)[0] === "correct") {
+      updatedKeys[Object.keys(letter)[0]] = "correct";
+      return;
+    }
+
     if (Object.values(letter)[0] === "present") {
       if (keys[Object.keys(letter)[0]] !== "correct") {
         updatedKeys[Object.keys(letter)[0]] = "present";
         return;
       }
     }
-    if (Object.values(letter)[0] === "correct") {
-      updatedKeys[Object.keys(letter)[0]] = "correct";
-      return;
+
+    if (keys[Object.keys(letter)[0]] === null) {
+      updatedKeys[Object.keys(letter)[0]] = "absent";
     }
   });
   return updatedKeys;
