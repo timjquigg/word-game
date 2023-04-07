ba4b89daff9215f63f6aa7e4aab14c225a249f5b
Tim Quigg <timjquigg@gmail.com>
2023-04-07 09:29:11 -0600

    Update type for letter rows in keyboard/index.tsx

--- components/search/keyboard/index.tsx
+++ components/search/keyboard/index.tsx
@@ -1,6 +1,6 @@
 import Key from "./key";
 
-const rows = [
+const rows: Letter[][] = [
   ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
   ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
   ["Z", "X", "C", "V", "B", "N", "M"],
